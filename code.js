a0d16dabde52572e3802d65cbe50768cb93cbf11
IvanoSarubbi <beyonce@thinkful.com>
2015-09-24 00:19:31 -0400

    changes

---
+++ code.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+     
+    for ( var i = 1; i < 21; i++){
+
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+
+    $('.solution').append('<p>FizzBuzz</p>');
+
+     }else{
+
+    if (i % 3 === 0) {
+
+       $('.solution').append('<p>Fizz</p>');
+
+    }
+    else if (i % 5 === 0) {
+
+        
+        $('.solution').append('<p>Buzz</p>');
+    }
+    else {
+     
+        $('.solution').append(i);
+    }
+
+}
+    
+     
+}
+});
